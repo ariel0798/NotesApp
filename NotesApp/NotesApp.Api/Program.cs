d7f488ff85c957fb1dc70e45cdc99b1e3c90eec0
ariel0798 <arielcruz0798@gmail.com>
2022-06-07 04:50:25 -0400

    Add MvcWebAppRegistrar

--- NotesApp/NotesApp.Api/Program.cs
+++ NotesApp/NotesApp.Api/Program.cs
@@ -6,17 +6,6 @@ builder.RegisterServices(typeof(Program));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.RegisterPipelineComponents(typeof(Program));
 
 app.Run();
