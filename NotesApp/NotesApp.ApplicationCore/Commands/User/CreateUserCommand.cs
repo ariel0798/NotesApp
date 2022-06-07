0ef87ec59de86c912c2c31b0ca93582ffeba1464
Richard Cruz <arielcruz0798@gmail.com>
2022-06-07 19:30:44 -0400

    Add CreateUser Command

---
+++ NotesApp/NotesApp.ApplicationCore/Commands/User/CreateUserCommand.cs
@@ -0,0 +1,19 @@
+using MediatR;
+using NotesApp.Domain.Models;
+
+namespace NotesApp.ApplicationCore.Commands.User;
+
+public class CreateUserCommand : IRequest<Domain.Models.User>
+{
+    public string Id{ get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; } 
+    public string PasswordSalt { get; set; } 
+    public string RefreshToken { get; set; }
+    public DateTime TokenCreated { get; set; } 
+    public DateTime TokenExpires { get; set; }
+    public DateTime UserCreated { get; set; } 
+    public string NoteId { get; set; } 
+}
