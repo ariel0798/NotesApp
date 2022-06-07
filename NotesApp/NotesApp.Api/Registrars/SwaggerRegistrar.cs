fd0ebee492a9335bcf53afd11db832e193b9ec38
ariel0798 <arielcruz0798@gmail.com>
2022-06-07 04:43:58 -0400

    Add SwaggerRegistrar

---
+++ NotesApp/NotesApp.Api/Registrars/SwaggerRegistrar.cs
@@ -0,0 +1,9 @@
+namespace NotesApp.Api.Registrars;
+
+public class SwaggerRegistrar : IWebApplicationBuilderRegistrar
+{
+    public void RegisterServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen();
+    }
+}
