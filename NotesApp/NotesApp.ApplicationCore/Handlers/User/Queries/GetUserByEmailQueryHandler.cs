5c21a771587133e104a1c837af6a781fb8b390c9
Richard Cruz <arielcruz0798@gmail.com>
2022-06-08 05:24:48 -0400

    Add GetUserByEmailQuery and its handler

---
+++ NotesApp/NotesApp.ApplicationCore/Handlers/User/Queries/GetUserByEmailQueryHandler.cs
@@ -0,0 +1,20 @@
+using MediatR;
+using NotesApp.ApplicationCore.Queries.User;
+using NotesApp.Domain.Interfaces;
+
+namespace NotesApp.ApplicationCore.Handlers.User.Queries;
+
+public class GetUserByEmailQueryHandler : IRequestHandler<GetUserByEmailQuery,Domain.Models.User>
+{
+    private readonly IUserRepository _userRepository;
+
+    public GetUserByEmailQueryHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    
+    public async Task<Domain.Models.User> Handle(GetUserByEmailQuery request, CancellationToken cancellationToken)
+    {
+        return await _userRepository.FindFirstByCondition(u => u.Email == request.email);
+    }
+}
