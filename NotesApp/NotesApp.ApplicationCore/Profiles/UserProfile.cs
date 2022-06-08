f61f741b2da3a1011fe092df98aaf1313a97a168
Richard Cruz <arielcruz0798@gmail.com>
2022-06-08 04:09:28 -0400

    Move User profile from Profiles.Commands to Profiles and delete Profile.Commands folder

--- NotesApp/NotesApp.ApplicationCore/Profiles/UserProfile.cs
+++ NotesApp/NotesApp.ApplicationCore/Profiles/UserProfile.cs
@@ -1,13 +1,15 @@
 using AutoMapper;
 using NotesApp.ApplicationCore.Commands.User;
+using NotesApp.ApplicationCore.Dtos.User;
 using NotesApp.Domain.Models;
 
-namespace NotesApp.ApplicationCore.Profiles.Commands;
+namespace NotesApp.ApplicationCore.Profiles;
 
 public class UserProfile : Profile
 {
     public UserProfile()
     {
         CreateMap<CreateUserCommand, User>();
+        CreateMap<RegisterUserDto, CreateUserCommand>();
     }
 }
