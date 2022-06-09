120081ca1ab990c0256a88b13b3cdc95607cc9d2
Richard Cruz <arielcruz0798@gmail.com>
2022-06-09 04:05:18 -0400

    Add VerifyPasswordHash Method on PasswordHashHelper

--- NotesApp/NotesApp.ApplicationCore/Helper/Interfaces/IPasswordHashHelper.cs
+++ NotesApp/NotesApp.ApplicationCore/Helper/Interfaces/IPasswordHashHelper.cs
@@ -3,4 +3,5 @@ namespace NotesApp.ApplicationCore.Helper.Interfaces;
 public interface IPasswordHashHelper
 {
     void CreatePasswordHash(string password, out string passwordHash, out string passwordSalt);
+    bool VerifyPasswordHash(string password, string passwordHash, string passwordSalt);
 }
--- NotesApp/NotesApp.ApplicationCore/Helper/PasswordHashHelper.cs
+++ NotesApp/NotesApp.ApplicationCore/Helper/PasswordHashHelper.cs
@@ -1,4 +1,6 @@
+using System.Buffers.Text;
 using System.Security.Cryptography;
+using System.Text;
 using System.Text.Json;
 using NotesApp.ApplicationCore.Helper.Interfaces;
 
@@ -15,8 +17,20 @@ public class PasswordHashHelper : IPasswordHashHelper
         passwordHash = Base64Encode(JsonSerializer.Serialize(hash));
         passwordSalt = Base64Encode(JsonSerializer.Serialize(salt));
     }
+    public bool VerifyPasswordHash(string password, string passwordHash, string passwordSalt)
+    {
+        using var hmac = new HMACSHA512(Base64Decode(passwordSalt));
+        var computerHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        return computerHash.SequenceEqual(Base64Decode(passwordHash));
+    }
     private static string Base64Encode(string plainText) {
         var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
         return Convert.ToBase64String(plainTextBytes);
+    }
+
+    private static byte[] Base64Decode(string base64Text)
+    {
+        var valueBytes = Convert.FromBase64String(base64Text);
+        return valueBytes;
     }
 }
