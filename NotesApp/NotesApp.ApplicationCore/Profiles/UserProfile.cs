0c1f5617c070297261a9ec2e0345b071bc9349ea
Richard Cruz <arielcruz0798@gmail.com>
2022-06-10 05:03:19 -0400

    Add JwtToken to SetUserTokenCommand map

--- NotesApp/NotesApp.ApplicationCore/Profiles/UserProfile.cs
+++ NotesApp/NotesApp.ApplicationCore/Profiles/UserProfile.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using NotesApp.ApplicationCore.Dtos.User;
+using NotesApp.ApplicationCore.Models;
 using NotesApp.ApplicationCore.Users.Commands;
 using NotesApp.Domain.Models;
 
@@ -11,5 +12,15 @@ public class UserProfile : Profile
     {
         CreateMap<CreateUserCommand, User>();
         CreateMap<RegisterUserDto, CreateUserCommand>();
+        CreateMap<JwtToken, SetUserTokenCommand>()
+            .ForMember(dest => dest.RefreshToken,
+                opt =>
+                    opt.MapFrom(src => src.RefreshToken))
+            .ForMember(dest => dest.TokenCreated,
+                opt =>
+                    opt.MapFrom(src => src.Created))
+            .ForMember(dest => dest.TokenExpires,
+                opt =>
+                    opt.MapFrom(src => src.Expires));
     }
 }
