0eb2a43f1cf9fa717974e91ff4bf5f2198e19231
Richard Cruz <arielcruz0798@gmail.com>
2022-06-13 05:43:37 -0400

    Add Authentication Registrar

---
+++ NotesApp/NotesApp.Api/Registrars/Builder/AuthenticationRegistrar.cs
@@ -0,0 +1,24 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace NotesApp.Api.Registrars.Builder;
+
+public class AuthenticationRegistrar : IWebApplicationBuilderRegistrar
+{
+    public void RegisterServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => 
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                        .GetBytes(builder.Configuration.GetSection("AppSetting:Token").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+    }
+}
