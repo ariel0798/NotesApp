653a4f43ba693f8a02478d5a8a3a791ef914b482
Richard Cruz <arielcruz0798@gmail.com>
2022-06-13 05:22:28 -0400

    Add CreateNote Command and its handler

---
+++ NotesApp/NotesApp.ApplicationCore/Notes/CommandHandlers/CreateNoteCommandHandler.cs
@@ -0,0 +1,27 @@
+using MediatR;
+using NotesApp.ApplicationCore.Notes.Commands;
+using NotesApp.Domain.Interfaces;
+using NotesApp.Domain.Models;
+
+namespace NotesApp.ApplicationCore.Notes.CommandHandlers;
+
+public class CreateNoteCommandHandler : IRequestHandler<CreateNoteCommand, Note>
+{
+    private readonly INoteRepository _noteRepository;
+
+    public CreateNoteCommandHandler(INoteRepository noteRepository)
+    {
+        _noteRepository = noteRepository;
+    }
+    public async Task<Note> Handle(CreateNoteCommand request, CancellationToken cancellationToken)
+    {
+        var note = new Note()
+        {
+            Notes = new List<NoteDetail>()
+        };
+
+        await _noteRepository.Create(note);
+        
+        return note;
+    }
+}
