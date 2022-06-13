e1db61d2dc37f47fb7f3494ab2f4e10d3f79f139
Richard Cruz <arielcruz0798@gmail.com>
2022-06-13 05:44:35 -0400

    Add Bearer authentication header to Swagger

--- NotesApp/NotesApp.Api/Registrars/Builder/SwaggerRegistrar.cs
+++ NotesApp/NotesApp.Api/Registrars/Builder/SwaggerRegistrar.cs
@@ -1,9 +1,23 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
 namespace NotesApp.Api.Registrars.Builder;
 
 public class SwaggerRegistrar : IWebApplicationBuilderRegistrar
 {
     public void RegisterServices(WebApplicationBuilder builder)
     {
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+            {
+                Description = "Standard Authorization header using the Bearer scheme (\"bearer token}\")",
+                In = ParameterLocation.Header,
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            });
+
+            options.OperationFilter<SecurityRequirementsOperationFilter>();
+        });
     }
 }
