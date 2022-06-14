8bcbee78750dd5eb207557257f7b927e9e773d9a
Richard Cruz <arielcruz0798@gmail.com>
2022-06-14 03:48:04 -0400

    Add NoteDetailId property to NoteDetail class

--- NotesApp/NotesApp.Domain/Models/NoteDetail.cs
+++ NotesApp/NotesApp.Domain/Models/NoteDetail.cs
@@ -2,6 +2,7 @@ namespace NotesApp.Domain.Models;
 
 public class NoteDetail
 {
+    public Guid NoteDetailId { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public DateTime LastEdited { get; set; } = DateTime.Now;
