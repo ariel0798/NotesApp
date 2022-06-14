9724698332b28462ded1633d8ba6110da4e05add
Richard Cruz <arielcruz0798@gmail.com>
2022-06-14 04:35:00 -0400

    Add private set to properties Email, PasswordHash, PasswordSalt and NoteId. Besides, Add their private methods to set the values

--- NotesApp/NotesApp.Domain/Models/User.cs
+++ NotesApp/NotesApp.Domain/Models/User.cs
@@ -7,24 +7,40 @@ public class User
 {
     [RedisIdField]
     public string Id{ get; set; }
+    
     public string Name { get; set; }
     public string LastName { get; set; }
     
     [Searchable(Sortable = true)]
-    public string Email { get; set; }
-    public string PasswordHash { get; set; } = string.Empty;
-    public string PasswordSalt { get; set; } = string.Empty;
+    public string Email { get; private set; }
+    public string PasswordHash { get; private set; }
+    public string PasswordSalt { get; private set; } 
     [Searchable(Sortable = true)]
     public string RefreshToken { get; private set; } 
     public DateTime TokenCreated { get; private set; } 
     public DateTime TokenExpires { get; private set; }
     public DateTime UserCreated { get; } = DateTime.Now;
-    public string NoteId { get; set; }
+    public string? NoteId { get; private set; }
 
     public void AddToken(string refreshToken, DateTime created, DateTime expires)
     {
         RefreshToken = refreshToken;
         TokenCreated = created;
         TokenExpires = expires;
+    }
+
+    public void AddEmail(string email)
+    {
+        Email = email;
+    }
+    public void SetPassword(string passwordHash, string passwordSalt)
+    {
+        PasswordHash = passwordHash;
+        PasswordSalt = passwordSalt;
+    }
+
+    public void AddNoteId(string noteId)
+    {
+        NoteId = noteId;
     }
 }
