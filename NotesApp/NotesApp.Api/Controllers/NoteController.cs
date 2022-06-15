72dbe0e4992ca0daafe9d7af6d3be444e8b2126a
Richard Cruz <arielcruz0798@gmail.com>
2022-06-15 06:02:17 -0400

    Add NoteController

---
+++ NotesApp/NotesApp.Api/Controllers/NoteController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NotesApp.ApplicationCore.Dtos.Note;
+using NotesApp.ApplicationCore.Services.NoteServices;
+
+namespace NotesApp.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class NoteController : Controller
+{
+    private readonly INoteService _noteService;
+
+    public NoteController(INoteService noteService)
+    {
+        _noteService = noteService;
+    }
+    
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> CreateNoteDetail(CreateNoteDto noteDto)
+    {
+        var result = await _noteService.CreateNote(noteDto);
+        return Ok(result);
+    }
+   
+}
