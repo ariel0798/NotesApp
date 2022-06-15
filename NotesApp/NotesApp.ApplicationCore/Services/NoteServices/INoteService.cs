6ee6b4ff883920a173a5999d763f912b4a539307
Richard Cruz <arielcruz0798@gmail.com>
2022-06-15 06:02:07 -0400

    Add NoteService with CreateNoteDetail Method and its interface

---
+++ NotesApp/NotesApp.ApplicationCore/Services/NoteServices/INoteService.cs
@@ -0,0 +1,9 @@
+using NotesApp.ApplicationCore.Dtos.Note;
+using NotesApp.Domain.Models;
+
+namespace NotesApp.ApplicationCore.Services.NoteServices;
+
+public interface INoteService
+{
+    Task<NoteDetail> CreateNote(CreateNoteDto noteDto);
+}
