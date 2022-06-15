aa7a214e7a7b8b6545ae2d6bbec53735c07f0e45
Richard Cruz <arielcruz0798@gmail.com>
2022-06-15 05:22:57 -0400

    Initialize NoteDetailId and set NoteDeleted property as null

--- NotesApp/NotesApp.Domain/Models/NoteDetail.cs
+++ NotesApp/NotesApp.Domain/Models/NoteDetail.cs
@@ -2,10 +2,10 @@ namespace NotesApp.Domain.Models;
 
 public class NoteDetail
 {
-    public Guid NoteDetailId { get; set; }
+    public Guid NoteDetailId { get; } = Guid.NewGuid();
     public string Title { get; set; }
     public string Description { get; set; }
     public DateTime LastEdited { get; set; } = DateTime.Now;
     public bool IsDeleted { get; set; } = false;
-    public DateTime NoteDeleted { get; set; }
+    public DateTime? NoteDeleted { get; set; } = null;
 }
