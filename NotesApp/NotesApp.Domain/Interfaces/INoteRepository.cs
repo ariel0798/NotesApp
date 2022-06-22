6a676387c1dc9c8ac2e2c89a248e7799963bc598
Richard Cruz <arielcruz0798@gmail.com>
2022-06-22 04:10:35 -0400

    Implement NoteRepository with StackExchange.Redis to be able to handle List<NoteDetails>

--- NotesApp/NotesApp.Domain/Interfaces/INoteRepository.cs
+++ NotesApp/NotesApp.Domain/Interfaces/INoteRepository.cs
@@ -2,7 +2,9 @@ using NotesApp.Domain.Models;
 
 namespace NotesApp.Domain.Interfaces;
 
-public interface INoteRepository :  IBaseRepository<Note>
+public interface INoteRepository
 {
-    
+    Task<Note> Create(Note obj);
+    Task<Note?> GetById(string id);
+    Task Update(Note obj);
 }
