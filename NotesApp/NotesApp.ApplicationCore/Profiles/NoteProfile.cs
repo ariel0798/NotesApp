28beae637778b3140c52f07a6cb4029fbd9f1b37
Richard Cruz <arielcruz0798@gmail.com>
2022-06-22 05:08:29 -0400

    Add Map from NoteDetail to GetNoteDetailDto

--- NotesApp/NotesApp.ApplicationCore/Profiles/NoteProfile.cs
+++ NotesApp/NotesApp.ApplicationCore/Profiles/NoteProfile.cs
@@ -11,5 +11,6 @@ public class NoteProfile : Profile
     {
         CreateMap<CreateNoteDto, CreateNoteDetailCommand>();
         CreateMap<CreateNoteDetailCommand, NoteDetail>();
+        CreateMap<NoteDetail, GetNoteDetailDto>(MemberList.Destination);
     }
 }
