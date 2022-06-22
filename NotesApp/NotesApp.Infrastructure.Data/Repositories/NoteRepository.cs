5b758d0229da50612970d5a1d45084105d6950fd
Richard Cruz <arielcruz0798@gmail.com>
2022-06-22 04:10:35 -0400

    Implement NoteRepository with StackExchange.Redis to be able to handle List<NoteDetails>

--- NotesApp/NotesApp.Infrastructure.Data/Repositories/NoteRepository.cs
+++ NotesApp/NotesApp.Infrastructure.Data/Repositories/NoteRepository.cs
@@ -1,12 +1,52 @@
+using System.Text.Json;
 using NotesApp.Domain.Interfaces;
 using NotesApp.Domain.Models;
-using Redis.OM;
+using StackExchange.Redis;
+using StackExchange.Redis.KeyspaceIsolation;
 
 namespace NotesApp.Infrastructure.Data.Repositories;
 
-public class NoteRepository : BaseRepository<Note> , INoteRepository
+public class NoteRepository :  INoteRepository
 {
-    public NoteRepository(RedisConnectionProvider provider) : base(provider)
+    private readonly IDatabase _database;
+
+    public NoteRepository(IConnectionMultiplexer redis)
     {
+        _database = redis.GetDatabase().WithKeyPrefix("Note");
     }
+
+    public async Task<Note> Create(Note obj)
+    {
+        var stringNote = JsonSerializer.Serialize(obj);
+
+        await _database.StringSetAsync(obj.Id, stringNote);
+        
+        return obj;
+    }
+
+    public async Task<Note?> GetById(string id)
+    {
+        var stringNote = await _database.StringGetAsync(id);
+
+        if(!string.IsNullOrEmpty(stringNote))
+        {
+            return JsonSerializer.Deserialize<Note>(stringNote.ToString());
+        }
+        
+        return null;
+    }
+
+    public async Task Update(Note obj)
+    {
+        var stringNote = JsonSerializer.Serialize(obj);
+
+        await _database.StringSetAsync(obj.Id, stringNote);
+    }
+
+    public async Task Remove(Note obj)
+    {
+        await _database.StringGetDeleteAsync(obj.Id);
+    }
+    
+    
 }
