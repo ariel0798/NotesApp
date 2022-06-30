b6ba5613b1d32cec1ac89c401c1615abc8b179e8
Richard Cruz <arielcruz0798@gmail.com>
2022-06-30 06:18:11 -0400

    Delete Application layer implementation of JwtHelper and PasswordHashHelper

--- NotesApp/NotesApp.ApplicationCore/Helpers/Interfaces/IPasswordHashHelper.cs
+++
@@ -1,7 +0,0 @@
-namespace NotesApp.ApplicationCore.Helpers.Interfaces;
-
-public interface IPasswordHashHelper
-{
-    void CreatePasswordHash(string password, out string passwordHash, out string passwordSalt);
-    bool VerifyPasswordHash(string password, string passwordHash, string passwordSalt);
-}
