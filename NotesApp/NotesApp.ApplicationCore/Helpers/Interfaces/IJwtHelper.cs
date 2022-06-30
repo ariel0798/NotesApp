7a9f1e58e53d6d108c6018b11eef82cb104ce4f4
Richard Cruz <arielcruz0798@gmail.com>
2022-06-30 06:18:11 -0400

    Delete Application layer implementation of JwtHelper and PasswordHashHelper

--- NotesApp/NotesApp.ApplicationCore/Helpers/Interfaces/IJwtHelper.cs
+++
@@ -1,9 +0,0 @@
-using NotesApp.ApplicationCore.Models;
-
-namespace NotesApp.ApplicationCore.Helpers.Interfaces;
-
-public interface IJwtHelper
-{
-    string CreateToken(string email);
-    JwtToken GenerateRefreshToken();
-}
