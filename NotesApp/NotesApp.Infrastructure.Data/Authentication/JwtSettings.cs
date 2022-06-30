15457ae90761fa138369ff1fc2cd4b6aa2944c5e
Richard Cruz <arielcruz0798@gmail.com>
2022-06-30 04:55:21 -0400

    Add JwtTokenGenerator and JwtSettings to infrastructure layer

---
+++ NotesApp/NotesApp.Infrastructure.Data/Authentication/JwtSettings.cs
@@ -0,0 +1,10 @@
+namespace NotesApp.Infrastructure.Data.Authentication;
+
+public class JwtSettings
+{
+    public const string SectionName = "JwtSettings";
+    public string Secret { get; init; } = null!;
+    public int ExpiryMinutes { get; set; }
+    public string Issuer { get; set; } = null!;
+
+}
