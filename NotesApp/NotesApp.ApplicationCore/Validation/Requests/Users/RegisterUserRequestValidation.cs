41896fc3a1d5b927ab1757f3d7e68d0317146739
Richard Cruz <arielcruz0798@gmail.com>
2022-07-08 03:44:51 -0400

    Delete duplicated email check

--- NotesApp/NotesApp.ApplicationCore/Validation/Requests/Users/RegisterUserRequestValidation.cs
+++ NotesApp/NotesApp.ApplicationCore/Validation/Requests/Users/RegisterUserRequestValidation.cs
@@ -1,48 +1,29 @@
 using FluentValidation;
-using MediatR;
 using NotesApp.ApplicationCore.Contracts.User.Requests;
-using NotesApp.ApplicationCore.Users.Queries;
+using NotesApp.Domain.Errors.Messages;
 
 namespace NotesApp.ApplicationCore.Validation.Requests.Users;
 
 public class RegisterUserRequestValidation : AbstractValidator<RegisterUserRequest>
 {
-    private readonly IMediator _mediator;
-
-    public RegisterUserRequestValidation(IMediator mediator)
+    public RegisterUserRequestValidation()
     {
-        _mediator = mediator;
-
         RuleFor(c => c.Email)
-            .NotEmpty()
+            .NotEmpty().WithMessage(Errors.Messages.User.EmptyName)
             .NotNull()
-            .EmailAddress()
-            .MustAsync(IsNotExistingUserEmail).WithMessage("Email already exists");
-        
+            .EmailAddress().WithMessage(Errors.Messages.User.InvalidEmailFormat);
+
         RuleFor(c => c.Password)
             .NotEmpty()
             .NotNull()
             .Length(7, 150);
-        
+
         RuleFor(c => c.Name)
-            .NotEmpty()
+            .NotEmpty().WithMessage(Errors.Messages.User.EmptyName)
             .NotNull();
         
         RuleFor(c => c.LastName)
             .NotEmpty()
             .NotNull();
-    }
-
-    private async Task<bool> IsNotExistingUserEmail(string email,CancellationToken cancellationToken)
-    {
-        if (email == null || email == string.Empty)
-            return false;
-        
-        var query = new GetUserByEmailQuery() { Email = email };
-
-        var user = await _mediator.Send(query,cancellationToken);
-
-        var isNotExistingUser = user == null;
-        return isNotExistingUser;
     }
 }
