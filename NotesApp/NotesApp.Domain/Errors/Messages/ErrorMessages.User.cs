11d4d595b42f7902014bdf4c3b71d4cd1e547c02
Richard Cruz <arielcruz0798@gmail.com>
2022-07-08 05:10:00 -0400

    Add user validation messages

--- NotesApp/NotesApp.Domain/Errors/Messages/ErrorMessages.User.cs
+++ NotesApp/NotesApp.Domain/Errors/Messages/ErrorMessages.User.cs
@@ -7,5 +7,12 @@ public static partial class ErrorMessages
         public const string DuplicatedEmail = "Email is already exist"; 
         public const string InvalidEmailFormat = "Invalid email format"; 
         public const string EmptyName = "'Name' must not be empty."; 
+        public const string NotNullName = "'Name' can not be null.";
+        public const string EmptyLastName = "'Last name' must not be empty."; 
+        public const string NotNullLastName = "'Last name' can not be null."; 
+        public const string EmptyPassword = "'Password' must not be empty."; 
+        public const string NotNullPassword = "'Password' must not be empty.";
+        public const string EmptyEmail= "'Email' must not be empty."; 
+        public const string NotNullEmail = "'Email' must not be empty."; 
     }
 }
