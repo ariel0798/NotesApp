ad18b89da37b80aaad73cc93843cf3c7c5d025ba
Richard Cruz <arielcruz0798@gmail.com>
2022-07-08 05:10:00 -0400

    Add user validation messages

--- NotesApp/NotesApp.ApplicationCore/Validation/Requests/Users/RegisterUserRequestValidation.cs
+++ NotesApp/NotesApp.ApplicationCore/Validation/Requests/Users/RegisterUserRequestValidation.cs
@@ -9,21 +9,21 @@ public class RegisterUserRequestValidation : AbstractValidator<RegisterUserReque
     public RegisterUserRequestValidation()
     {
         RuleFor(c => c.Email)
-            .NotEmpty().WithMessage(ErrorMessages.User.EmptyName)
-            .NotNull()
+            .NotEmpty().WithMessage(ErrorMessages.User.EmptyEmail)
+            .NotNull().WithMessage(ErrorMessages.User.NotNullEmail)
             .EmailAddress().WithMessage(ErrorMessages.User.InvalidEmailFormat);
 
         RuleFor(c => c.Password)
-            .NotEmpty()
-            .NotNull()
+            .NotEmpty().WithMessage(ErrorMessages.User.EmptyPassword)
+            .NotNull().WithMessage(ErrorMessages.User.NotNullPassword)
             .Length(7, 150);
 
         RuleFor(c => c.Name)
             .NotEmpty().WithMessage(ErrorMessages.User.EmptyName)
-            .NotNull();
+            .NotNull().WithMessage(ErrorMessages.User.NotNullName);
         
         RuleFor(c => c.LastName)
-            .NotEmpty()
-            .NotNull();
+            .NotEmpty().WithMessage(ErrorMessages.User.EmptyLastName)
+            .NotNull().WithMessage(ErrorMessages.User.NotNullLastName);
     }
 }
