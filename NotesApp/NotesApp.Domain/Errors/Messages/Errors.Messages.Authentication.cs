c1ee9e58edc287bee6ef517806df9130e67e1e9c
Richard Cruz <arielcruz0798@gmail.com>
2022-07-08 03:43:16 -0400

    Add Errors.Messages.Authentication and Errors.Messages.User

---
+++ NotesApp/NotesApp.Domain/Errors/Messages/Errors.Messages.Authentication.cs
@@ -0,0 +1,13 @@
+namespace NotesApp.Domain.Errors.Messages;
+
+public static partial class Errors
+{
+    public static partial class Messages
+    {
+        public static class Authentication
+        
+        {
+            public const string InvalidCredentials = "Invalid Credentials"; 
+        }
+    }
+}
