1b7b299a64cba9dfc8c1a2cfa4ef2b3841d662e8
Richard Cruz <arielcruz0798@gmail.com>
2022-07-08 04:13:49 -0400

    Add ErrorMessages.Note

---
+++ NotesApp/NotesApp.Domain/Errors/Messages/ErrorMessages.Note.cs
@@ -0,0 +1,9 @@
+namespace NotesApp.Domain.Errors.Messages;
+
+public static partial class ErrorMessages
+{
+    public static class Note
+    {
+        public const string NoteNotFound = "The note was not found"; 
+    }
+}
