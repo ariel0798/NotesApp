1085f663c5339ad01403f63feda7956298423484
Richard Cruz <arielcruz0798@gmail.com>
2022-07-11 02:39:03 -0400

    Add implementation to UserRepository

--- NotesApp/NotesApp.Domain/Interfaces/IUserRepository.cs
+++ NotesApp/NotesApp.Domain/Interfaces/IUserRepository.cs
@@ -1,8 +1,13 @@
+using System.Linq.Expressions;
 using NotesApp.Domain.Models;
 
 namespace NotesApp.Domain.Interfaces;
 
-public interface IUserRepository : IBaseRepository<User>
+public interface IUserRepository
 {
-    
+    Task<User> Create(User obj);
+    Task<User?> GetById(string id);
+    Task<IEnumerable<User>> GetAll();
+    Task Update(User obj);
+    Task Remove(User obj);
 }
