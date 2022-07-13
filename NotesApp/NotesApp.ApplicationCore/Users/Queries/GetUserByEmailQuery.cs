8c1c0e3f7e3f7dd4080d6002b7edb9ba77ec28dd
Richard Cruz <arielcruz0798@gmail.com>
2022-07-13 04:20:36 -0400

    Remove necessaries classes

--- NotesApp/NotesApp.ApplicationCore/Users/Queries/GetUserByEmailQuery.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-using NotesApp.Domain.Models;
-
-namespace NotesApp.ApplicationCore.Users.Queries;
-
-public class GetUserByEmailQuery : IRequest<User>
-{
-    public string Email { get; set; }
-}
--- NotesApp/NotesApp.ApplicationCore/Users/Queries/GetUserByRefreshTokenQuery.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-using NotesApp.Domain.Models;
-
-namespace NotesApp.ApplicationCore.Users.Queries;
-
-public class GetUserByRefreshTokenQuery : IRequest<User>
-{
-    public string RefreshToken { get; set; }
-}
