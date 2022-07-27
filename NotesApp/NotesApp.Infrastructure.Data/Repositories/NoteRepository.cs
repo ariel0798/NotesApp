9e405e2d5946330edbf95bd7b5ef962c748eea3a
Richard Cruz <arielcruz0798@gmail.com>
2022-07-26 22:30:09 -0400

    Add GetAll Notes method

--- NotesApp/NotesApp.Infrastructure.Data/Repositories/NoteRepository.cs
+++ NotesApp/NotesApp.Infrastructure.Data/Repositories/NoteRepository.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using NotesApp.Domain.Interfaces;
 using NotesApp.Domain.Models;
 using StackExchange.Redis;
@@ -8,11 +9,14 @@ namespace NotesApp.Infrastructure.Data.Repositories;
 
 public class NoteRepository :  INoteRepository
 {
+    private const string Prefix = "Note";
     private readonly IDatabase _database;
-
+    private readonly RedisKey[] _keys;
     public NoteRepository(IConnectionMultiplexer redis)
     {
-        _database = redis.GetDatabase().WithKeyPrefix("Note");
+        _database = redis.GetDatabase().WithKeyPrefix(Prefix);
+        var endPoint = redis.GetEndPoints().First();
+        _keys = redis.GetServer(endPoint).Keys(pattern: Prefix + ":*").ToArray();
     }
 
     public async Task<Note> Create(Note obj)
@@ -30,10 +34,24 @@ public class NoteRepository :  INoteRepository
 
         if(!string.IsNullOrEmpty(stringNote))
         {
-            return JsonSerializer.Deserialize<Note>(stringNote.ToString());
+            return DeserializeNote(stringNote);
         }
         
         return null;
+    }
+
+    public async Task<IQueryable<Note>> GetAll()
+    {
+        var noteList = new List<Note>();
+
+        foreach (var key in _keys)
+        {
+            var keyString = key.ToString().Replace(Prefix,"");
+            var stringNote = await _database.StringGetAsync(keyString);
+            noteList.Add(DeserializeNote(stringNote));
+        }
+
+        return noteList.AsQueryable();
     }
 
     public async Task Update(Note obj)
@@ -51,5 +69,10 @@ public class NoteRepository :  INoteRepository
     private string AppendColon(string id)
     {
         return ":" + id;
+    }
+
+    private Note? DeserializeNote(string stringNote)
+    {
+        return JsonSerializer.Deserialize<Note>(stringNote.ToString());
     }
 }
