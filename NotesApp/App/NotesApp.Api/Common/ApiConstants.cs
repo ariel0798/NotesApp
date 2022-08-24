8f21994c6bf4c9401deba4cb014d9071fbb994c2
Richard Cruz <arielcruz0798@gmail.com>
2022-08-24 04:39:17 -0400

    Add Notes Urls and constants to ApiConstants

--- NotesApp/App/NotesApp.Api/Common/ApiConstants.cs
+++ NotesApp/App/NotesApp.Api/Common/ApiConstants.cs
@@ -16,4 +16,18 @@ public static class ApiConstants
             public const string RefreshToken = $"{BaseRoute}/refresh-token";
         }
     }
+    public static class Notes
+    {
+        public const string Tag = "Notes";
+        public const string BaseRoute = "notes";
+
+        public static class EndpointNames
+        {
+            public const string NoteDetailId = $"{BaseRoute}/{{noteDetailId}}";
+            public const string Trash = $"{BaseRoute}/trash";
+            public const string Recover = $"{BaseRoute}/{{noteDetailId}}/recover";
+            public const string SoftDeleteByNoteDetailId = $"{BaseRoute}/{{noteDetailId}}/soft-delete";
+            public const string RecurringJobInitializer = "/recurringJob-initializer";
+        }
+    }
 }
