962d4d520d6b1e9e123e343d7304bf19e3b1d641
Richard Cruz <arielcruz0798@gmail.com>
2022-08-24 20:57:32 -0400

    Add Expected response GetNoteDetailByIdEndpointSummary

--- NotesApp/App/NotesApp.Api/Summaries/Notes/GetNoteDetailByIdEndpointSummary.cs
+++ NotesApp/App/NotesApp.Api/Summaries/Notes/GetNoteDetailByIdEndpointSummary.cs
@@ -1,6 +1,7 @@
 using NotesApp.Api.Common;
 using NotesApp.Api.Endpoints.Notes;
 using NotesApp.Api.Extensions;
+using NotesApp.ApplicationCore.Contracts.Notes.Responses;
 
 namespace NotesApp.Api.Summaries.Notes;
 
@@ -9,6 +10,7 @@ public class GetNoteDetailByIdEndpointSummary : ISummary<GetNoteDetailByIdEndpoi
     public static RouteHandlerBuilder SetSummary(RouteHandlerBuilder builder)
     {
         return builder
+            .Response<NoteDetailResponse>(StatusCodes.Status200OK, "Returns note")
             .Summary("Get Note detail by noteDetailId","Get Note detail by noteDetailId")
             .WithTags(ApiConstants.Notes.Tag);
 
